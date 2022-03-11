5abbc6d9f5d9dcb84a395846c1f7eee7ab7914cf
unknown <agordillos@gmail.com>
2022-03-11 10:23:19 +0100

    CSS first version

--- apps/strongbox/app/components/MainScreen.jsx
+++ apps/strongbox/app/components/MainScreen.jsx
@@ -6,23 +6,45 @@ import './../assets/scss/main.scss';
 export default class MainScreen extends React.Component {
   constructor(props){
     super(props);
-  }
+  };
+
+  // componentDidMount() {
+  //   window.addEventListener('resize', this.handleResize);
+  // }
+  // componentWillUnmount() {
+  //   window.removeEventListener('resize', this.handleResize);
+  // }
+
+  // handleResize(){
+  //   var scale = Math.min($("#container").width() / 690, $("#container").height() / 690);
+  //   var newWidth = 70*scale;
+  //   var newHeight = 70*scale;
+  //   $("div.boxButton").width(newWidth).height(newHeight);
+  // }
 
   render(){
     return (
       <div id="container">
-        <BoxButton value={"1"} position={1}/>
-        <BoxButton value={"2"} position={2}/>
-        <BoxButton value={"3"} position={3}/>
-        <BoxButton value={"4"} position={4}/>
-        <BoxButton value={"5"} position={5}/>
-        <BoxButton value={"6"} position={6}/>
-        <BoxButton value={"7"} position={7}/>
-        <BoxButton value={"8"} position={8}/>
-        <BoxButton value={"9"} position={9}/>
-        <BoxButton value={"*"} position={10}/>
-        <BoxButton value={"0"} position={11}/>
-        <BoxButton value={"#"} position={12}/>
+        <div id="row1" className="row">
+          <BoxButton value={"1"} position={1}/>
+          <BoxButton value={"2"} position={2}/>
+          <BoxButton value={"3"} position={3}/>
+        </div>
+        <div id="row2" className="row">
+          <BoxButton value={"4"} position={4}/>
+          <BoxButton value={"5"} position={5}/>
+          <BoxButton value={"6"} position={6}/>
+        </div>
+        <div id="row3" className="row">
+          <BoxButton value={"7"} position={7}/>
+          <BoxButton value={"8"} position={8}/>
+          <BoxButton value={"9"} position={9}/>
+        </div>
+        <div id="row4" className="row">
+          <BoxButton value={"*"} position={10}/>
+          <BoxButton value={"0"} position={11}/>
+          <BoxButton value={"#"} position={12}/>
+        </div>
       </div>
     );
   }
