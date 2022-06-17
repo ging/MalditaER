ca31c15de3c3bef61a6fa83507a7ebf006199d4e
unknown <agordillos@gmail.com>
2022-06-17 11:56:32 +0200

    Webswitcher with all ediphy docs

--- apps/webswitcher/app/config/config.js
+++ apps/webswitcher/app/config/config.js
@@ -16,23 +16,28 @@ export let GLOBAL_CONFIG = {
   localStorageKey:"WEB_SWITCHER_2022",
   webs:{
     es:[
-      {name:"Ediphy MalditaER (I)", url:"https://ging.github.io/MalditaER/video", switchOnPuzzle:0},
-      {name:"Ediphy MalditaER (I)", url:"https://vishub.org/ediphy_documents/411.full?nowatermark=1", switchOnPuzzle:1},
-      {name:"Ediphy MalditaER (Full)", url:"https://vishub.org/ediphy_documents/402.full?nowatermark=1", switchOnPuzzle:2},
-      {name:"Ediphy MalditaER (III)", url:"https://vishub.org/ediphy_documents/416.full?nowatermark=1", switchOnPuzzle:8}
+      {name:"Video", url:"https://ging.github.io/MalditaER/video", switchOnPuzzle:1},
+      {name:"Ediphy MalditaER (I)", url:"https://vishub.org/ediphy_documents/422.full?nowatermark=1", switchOnPuzzle:2},
+      {name:"Ediphy MalditaER (II)", url:"https://vishub.org/ediphy_documents/421.full?nowatermark=1", switchOnPuzzle:3},
+      {name:"Ediphy MalditaER (III)", url:"https://vishub.org/ediphy_documents/420.full?nowatermark=1", switchOnPuzzle:4},
+      {name:"Ediphy MalditaER (IV)", url:"https://vishub.org/ediphy_documents/419.full?nowatermark=1", switchOnPuzzle:5},
+      {name:"Ediphy MalditaER (Fin)", url:"https://vishub.org/ediphy_documents/416.full?nowatermark=1", switchOnPuzzle:8}
     ],
     en:[
-      {name:"Ediphy MalditaER (I)", url:"https://ging.github.io/MalditaER/video", switchOnPuzzle:0},
-      {name:"Ediphy MalditaER (I)", url:"https://vishub.org/ediphy_documents/411.full?nowatermark=1", switchOnPuzzle:1},
-      {name:"Ediphy MalditaER (Full)", url:"https://vishub.org/ediphy_documents/402.full?nowatermark=1", switchOnPuzzle:2},
-      {name:"Ediphy MalditaER (III)", url:"https://vishub.org/ediphy_documents/416.full?nowatermark=1", switchOnPuzzle:8}
-
+      {name:"Video", url:"https://ging.github.io/MalditaER/video", switchOnPuzzle:1},
+      {name:"Ediphy MalditaER (I)", url:"https://vishub.org/ediphy_documents/422.full?nowatermark=1", switchOnPuzzle:2},
+      {name:"Ediphy MalditaER (II)", url:"https://vishub.org/ediphy_documents/421.full?nowatermark=1", switchOnPuzzle:3},
+      {name:"Ediphy MalditaER (III)", url:"https://vishub.org/ediphy_documents/420.full?nowatermark=1", switchOnPuzzle:4},
+      {name:"Ediphy MalditaER (IV)", url:"https://vishub.org/ediphy_documents/419.full?nowatermark=1", switchOnPuzzle:5},
+      {name:"Ediphy MalditaER (Fin)", url:"https://vishub.org/ediphy_documents/416.full?nowatermark=1", switchOnPuzzle:8}
     ],
     it:[
-      {name:"Ediphy MalditaER (I)", url:"https://ging.github.io/MalditaER/video", switchOnPuzzle:0},
-      {name:"Ediphy MalditaER (I)", url:"https://vishub.org/ediphy_documents/411.full?nowatermark=1", switchOnPuzzle:1},
-      {name:"Ediphy MalditaER (Full)", url:"https://vishub.org/ediphy_documents/402.full?nowatermark=1", switchOnPuzzle:2},
-      {name:"Ediphy MalditaER (III)", url:"https://vishub.org/ediphy_documents/416.full?nowatermark=1", switchOnPuzzle:8}
+      {name:"Video", url:"https://ging.github.io/MalditaER/video", switchOnPuzzle:1},
+      {name:"Ediphy MalditaER (I)", url:"https://vishub.org/ediphy_documents/422.full?nowatermark=1", switchOnPuzzle:2},
+      {name:"Ediphy MalditaER (II)", url:"https://vishub.org/ediphy_documents/421.full?nowatermark=1", switchOnPuzzle:3},
+      {name:"Ediphy MalditaER (III)", url:"https://vishub.org/ediphy_documents/420.full?nowatermark=1", switchOnPuzzle:4},
+      {name:"Ediphy MalditaER (IV)", url:"https://vishub.org/ediphy_documents/419.full?nowatermark=1", switchOnPuzzle:5},
+      {name:"Ediphy MalditaER (Fin)", url:"https://vishub.org/ediphy_documents/416.full?nowatermark=1", switchOnPuzzle:8}
     ],
   },
   escapp:{
