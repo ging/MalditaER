897c9993982745432df80c7fe58921091876105d
Enrique <enrique.barra@upm.es>
2022-06-27 16:22:19 +0100

    fakedetector en inglés e infografías

--- apps/fakedetector/src/config/news.js
+++ apps/fakedetector/src/config/news.js
@@ -69,37 +69,34 @@ export let NEWSit = [
 
 export let NEWSen = [
   {
-    "path": "./assets/images/1.jpg",
-    "source_url": "",
+    "path": "./assets/images/en/1.jpg",    
+    "comment": "Are these men refugees coming to England during the Syrian refugees crisis in 2015?",
     "true_or_false": false,
-    "refutation_url": "https://maldita.es/malditobulo/20190821/no-esta-foto-no-ha-sido-tomada-en-el-open-arms-es-de-asistentes-a-un-festival-de-londres-en-2015/",
-    "refutation_text": "Bulo. La foto es de un festival de música en Londres: uno de los hombres de la imagen la subió a su cuenta de Instagram en 2015. Es fácil desinformar con una imagen descontextualizada.",
+    "feedback": "No es un barco, es un festival de música, pero seguro que la gente se lo cree."
   },
   {
-    "path": "./assets/images/2.jpg",
-    "source_url": "https://www.telegraph.co.uk/news/2016/09/07/gas-tanks-found-in-unmarked-car-by-paris-notre-dame-cathedral-sp",
+    "path": "./assets/images/en/2.jpg",
     "true_or_false": false,
-    "refutation_url": "https://maldita.es/malditobulo/bulos-y-desinformaciones-sobre-el-incendio-de-la-catedral-de-notre-dame-de-paris",
-    "refutation_text": "El incendio de Notre Dame del que habla el tuit ocurrió en 2019, pero la noticia que comparte es de 2016. Recuerda que descontextualizar en el tiempo también es desinformar.",
+    "feedback": "Los tuits falsos en nombre de medios de comunicación siempre funcionan."
   },
   {
-    "path": "./assets/images/3.png",
-    "source_url": "",
+    "path": "./assets/images/en/3.jpg",
     "true_or_false": true,
+    "feedback": "Esto es verdad y se cuenta cuándo y dónde lo dijo. En nuestros bulos no hay que dar esos datos."
   },
   {
-    "path": "./assets/images/4.jpg",
-    "source_url": "",
+    "path": "./assets/images/en/4.jpg",
     "true_or_false": false,
+    "feedback": "Este bulo ha funcionado en otros países  así que vamos a probarlo aquí."
   },
   {
-    "path": "./assets/images/5.png",
-    "source_url": "",
+    "path": "./assets/images/en/5.png",
     "true_or_false": false,
+    "feedback": "Una foto y una frase llamativa sirven para desinformar, vamos a usarla"
   },
   {
-    "path": "./assets/images/6.png",
-    "source_url": "",
+    "path": "./assets/images/en/6.png",
     "true_or_false": true,
+    "feedback": "Esto es verdad y hay un vídeo oficial. Para hacer un bulo, cuantas menos pruebas mejor"
   }
 ]
