2d4cfe30043562c1af81391c7e52aee46a95f673
Enrique <enrique.barra@upm.es>
2022-06-28 12:21:08 +0100

    fake detector y fake mail production

--- apps/fakedetector/src/config/news.js
+++ apps/fakedetector/src/config/news.js
@@ -72,31 +72,33 @@ export let NEWSen = [
     "path": "./assets/images/en/1.jpg",    
     "comment": "Are these men refugees coming to England during the Syrian refugees crisis in 2015?",
     "true_or_false": false,
-    "feedback": "No es un barco, es un festival de música, pero seguro que la gente se lo cree."
+    "feedback": "In the background of the images is only the sea. It could be any place, so if we told them that it’s the United Kingdom, I’m sure they would believe that."
   },
   {
     "path": "./assets/images/en/2.jpg",
+    "comment": "There was a fire in Notre Dame and people tweeted that:",
     "true_or_false": false,
-    "feedback": "Los tuits falsos en nombre de medios de comunicación siempre funcionan."
+    "feedback": "This news is old, but to check it out they have to look closely. If we tweet it as if it were current, they will believe it."
   },
   {
     "path": "./assets/images/en/3.jpg",
     "true_or_false": true,
-    "feedback": "Esto es verdad y se cuenta cuándo y dónde lo dijo. En nuestros bulos no hay que dar esos datos."
+    "feedback": "This is real news and there is an official video. To create a hoax, the less evidence we provide, the better."
   },
   {
     "path": "./assets/images/en/4.jpg",
     "true_or_false": false,
-    "feedback": "Este bulo ha funcionado en otros países  así que vamos a probarlo aquí."
+    "feedback": "This hoax has worked in other countries, so let's try it out here."
   },
   {
-    "path": "./assets/images/en/5.png",
+    "path": "./assets/images/en/5.jpg",
+    "comment": "Are they Europeans trying to get to North Africa during World War II?",
     "true_or_false": false,
-    "feedback": "Una foto y una frase llamativa sirven para desinformar, vamos a usarla"
+    "feedback": "Changing the photo to black and white makes it look old. Even if it's from 1991, we are going to say it was taken during the Second World War."
   },
   {
     "path": "./assets/images/en/6.png",
     "true_or_false": true,
-    "feedback": "Esto es verdad y hay un vídeo oficial. Para hacer un bulo, cuantas menos pruebas mejor"
+    "feedback": "That is real and the date of the statement appears. In a hoax it is better that no data appears so that it is more difficult to verify it."
   }
 ]
